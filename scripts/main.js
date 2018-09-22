9c83243f9d7b8030e9268dfa8fa8a92739a866ca
Gisbel <gisbel94@gmail.com>
2018-09-22 16:43:22 -0300

    agregando archivos a mi repositorio

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+var miImage = document.querySelector('img');
+
+miImage.onclick = function () {
+    var miSrc = miImage.getAttribute('src');
+    if (miSrc === 'images/firefox-icon.png') {
+      miImage.setAttribute('src','images/firefox2.png');
+    } else {
+      miImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
